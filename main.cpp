a9c2bb6ca0dc54ac9b711c6e737344c90e72e18f
daniiul <dmalakhov2005@gmail.com>
2024-12-03 20:38:05 -0800

    PART 1 COMPLETE

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 using namespace std;
 
 
+int sum_ascii(string str);
+
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    
+    int as = sum_ascii("536B9DFC93AF");
+    cout << "The ascii value of '536B9DFC93AF' is: " << as << endl;
 
     return 0;
 }
 
-/* 
+int sum_ascii(string str)
+{
+    int sum = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        sum += (int) str[i];
+    }
+    return sum;
+}
+
+/*
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF
 1DA9D64D02A0
